79db8577e07a5e228ca35dc7512a09251b6e9108
ROGAN07sg <shekhargupta349@gmail.com>
2026-02-27 11:12:11 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello uni!";
+    private static final String MESSAGE = "Hello universe!";
 
     public App() {}
 
